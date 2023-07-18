99edecfc031836afaa5f1f1c1fd11009f51385de
Gabriel <emilio.nunez.arenas@gmail.com>
2023-07-17 20:48:47 -0500

    fixing problems with divergent branches

--- get-that-home/src/App.jsx
+++ get-that-home/src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Form from "./views/form"
 import LandingPage from "./pages/landing-page"
 
 function App() {
-
   return (
-    <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage/>} />
+          <Route path="/form" element={<Form/>} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
