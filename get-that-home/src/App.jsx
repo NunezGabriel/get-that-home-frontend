a32beb1b29ac8d47fe1ed25668dc67649452702b
pdroariasvilela <pdroariasvilela@gmail.com>
2023-07-18 09:39:24 -0500

    update:propert-not-logged

--- get-that-home/src/App.jsx
+++ get-that-home/src/App.jsx
@@ -1,11 +1,15 @@
-import PropertyNotLogged from "./components/property-not-logged"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Form from "./views/form"
+import LandingPage from "./pages/landing-page"
 
 function App() {
-
   return (
-    <div>
-      <PropertyNotLogged />
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage/>} />
+          <Route path="/form" element={<Form/>} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
