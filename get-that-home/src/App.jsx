4edf904e9ab670d87686a75d2cccae802d5d1e91
Gabriel <emilio.nunez.arenas@gmail.com>
2023-07-17 20:41:35 -0500

    solving comflicts

--- get-that-home/src/App.jsx
+++ get-that-home/src/App.jsx
@@ -1,13 +1,9 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-import Form from "./views/form"
+import LandingPage from "./pages/landing-page"
 
 function App() {
+
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/form" element={<Form/>} />
-        </Routes>
-      </BrowserRouter>
+    <LandingPage />
   )
 }
 
