e2bbf9252f181c643824b4a2d3139f8e173b03ec
Gabriel <emilio.nunez.arenas@gmail.com>
2023-07-19 12:40:51 -0500

    add: modals 100%

---
+++ get-that-home/src/components/modals/more-modal.jsx
@@ -0,0 +1,45 @@
+import { AiOutlineMinus } from 'react-icons/ai';
+
+import { SimpleFlexContainer, FlexContainerL } from "../../pages/form";
+import { CheckboxInput, MainInput } from "../input";
+import SimpleText from "../simpleText";
+import { ModalBody, DoneContainer, PriceSectionContainer } from "./price-modal";
+import { SimpleContainer } from "../../pages/form";
+import Label from "../label";
+import { LoginButton, JoinButton } from "../button";
+
+const MoreModal = () => {
+    return ( 
+        <ModalBody>
+            <SimpleContainer>
+                <SimpleFlexContainer>
+                    <FlexContainerL>
+                        <CheckboxInput type="checkbox"/>
+                        <SimpleText>
+                            Houses
+                        </SimpleText>
+                    </FlexContainerL>
+                </SimpleFlexContainer>
+                <div style={{padding: "10px"}}>
+                    <Label> price range</Label>
+                    <PriceSectionContainer>
+                        <JoinButton>
+                            <MainInput placeholder="min"/>
+                        </JoinButton>
+                        <AiOutlineMinus color="#8E8E8E" fontSize={50}/>
+                        <JoinButton>
+                            <MainInput placeholder="max"/>
+                        </JoinButton>
+                    </PriceSectionContainer>
+                </div>
+                <DoneContainer>
+                    <LoginButton>
+                        DONE
+                    </LoginButton>
+                </DoneContainer>
+            </SimpleContainer>
+        </ModalBody>
+    );
+}
+ 
+export default MoreModal;
