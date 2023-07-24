ee492708d73c3260cc64939caa0c9fe6e5bd4385
Gabriel <emilio.nunez.arenas@gmail.com>
2023-07-24 16:03:31 -0500

    chore: all filters

--- get-that-home/src/service/api-fetch.jsx
+++ get-that-home/src/service/api-fetch.jsx
@@ -30,7 +30,6 @@ export default async function apiFetch(
   const response = await fetch(BASE_URI + endpoint, config);
 
   let data;
-  // console.log(response);
   if (!response.ok) {
     try {
       data = await response.json();
